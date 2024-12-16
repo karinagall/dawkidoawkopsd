3d5ecf90f133bde538b9931edd646b63fc8bb41f
karinagall <imnami71@gmail.com>
2024-12-16 15:28:59 +0900

    Update cytube.js

--- cytube.js
+++ cytube.js
@@ -81,7 +81,7 @@ $(document).ready(function() {
 let btnopt_html = `
 <button id="btn_chat_opt"></button>
 <div id="popup_chatmenu" class="popup_chatmenu">
-    <div class="popup_title">
+    <div class="popup_title"><div style='background-color: #00ff00'>
         <h6>중계방 설정</h6>
         <button id="btn_chatmenu_close" class="btn_chatmenu_close">닫기</button>
     </div>
@@ -109,7 +109,6 @@ let btnopt_html = `
          </li>
          
 	</ul>
-<div style='background-color: #00ff00'>
 </div>
 
 
